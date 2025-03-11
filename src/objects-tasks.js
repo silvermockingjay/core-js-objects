7deecc4c3b84788adb2abc6ad6f0ef2133f3d017
anakdrch <ana.kudrevich@gmail.com>
2025-03-11 17:35:59 +0100

    feat: implement sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -260,8 +260,14 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  arr.sort((a, b) => {
+    if (a.country === b.country) {
+      return a.city.localeCompare(b.city);
+    }
+    return a.country.localeCompare(b.country);
+  });
+  return arr;
 }
 
 /**
